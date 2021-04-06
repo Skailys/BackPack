64f7e05b0f4183cfba9804b2a2f0dbcf36528d68
fredie04 <62189639+fredie04@users.noreply.github.com>
2021-04-06 11:47:08 +0200

    Fix dropping state & expand of plugin.yml (#2)

--- src/main/java/de/fredie1104/projects/backpacks/listener/ModifyShulker.java
+++ src/main/java/de/fredie1104/projects/backpacks/listener/ModifyShulker.java
@@ -27,6 +27,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.*;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
@@ -257,5 +258,11 @@ public class ModifyShulker implements Listener {
         }
 
         e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent e) {
+        Player p = e.getEntity();
+        p.closeInventory();
     }
 }
